52f1c5ca2c166e22e8aaa589a69d5c355824c608
Evgeny <art33@list.ru>
2022-09-14 11:40:53 +0300

    2 commit

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -9,98 +9,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService{
-    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
-    private final Connection connection = Util.connection();
+   static UserDaoJDBCImpl userDaoJDBC;
+
+    static {
+        try {
+            userDaoJDBC = new UserDaoJDBCImpl();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 
     public UserServiceImpl() throws SQLException, ClassNotFoundException {
 
     }
 
-    public void createUsersTable() {
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate("create table if not exists test.users (id int not null auto_increment," +
-                    " name VARCHAR(255) not null,  lastName VARCHAR(255) not null, age int not null, " +
-                    "PRIMARY KEY (`id`))");
-            System.out.println("Таблица создана!");
 
-        } catch (SQLException e) {
-            System.out.println("Таблица создана ранее!");
-        }
+    public void createUsersTable() {
+
+
+        userDaoJDBC.createUsersTable();
 
 
 
     }
 
     public void dropUsersTable() {
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate("Drop table if exists test.users");
-            System.out.println("Таблица удалена");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+
+        userDaoJDBC.dropUsersTable();
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
 
-        String sql = "INSERT INTO test.users(name, lastname, age) VALUES(?,?,?)";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.setByte(3, age);
-            preparedStatement.executeUpdate();
-            System.out.println("User с именем – " + name + " добавлен в базу данных");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+     userDaoJDBC.saveUser(name, lastName, age);
 
     }
 
     public void removeUserById(long id) {
-        try (Statement statement = connection.createStatement()) {
-            String sql = "DELETE FROM test.users where id";
-            statement.executeUpdate(sql);
-            System.out.println("User удален");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+
+        userDaoJDBC.removeUserById(id);
 
     }
 
     public List<User> getAllUsers () {
-        List<User> allUser = new ArrayList<>();
-
-        String sql = "SELECT id, name, lastName, age from test.users";
-
-        try (Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()) {
-                User user = new User();
-                user.setId(resultSet.getLong("id"));
-                user.setName(resultSet.getString("name"));
-                user.setLastName(resultSet.getString("lastName"));
-                user.setAge(resultSet.getByte("age"));
-                System.out.println(user.toString());
-                allUser.add(user);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-//
-        }
-        return allUser;
+              
+        return userDaoJDBC.getAllUsers();
 
     }
 
     public void cleanUsersTable () {
-        String sql = "DELETE FROM test.users";
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate(sql);
-            System.out.println("Таблица очищена");
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Не удалось очистить");
-        }
+
+        userDaoJDBC.cleanUsersTable();
 
     }
 
